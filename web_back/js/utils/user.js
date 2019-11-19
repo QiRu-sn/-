49e52748aad310d4ffeb8d7d49da380b97787188
QiRu <916331669@qq.com>
2019-11-19 20:58:58 +0800

    管理员登出功能实现及优化

--- web_back/js/utils/user.js
+++ web_back/js/utils/user.js
@@ -2,14 +2,25 @@
 var URL = 'http://localhost:8000';
 var user = {
     // 登录请求
-    login: function(myName, myPwd) {
+    login: function(myName, myPwd, callback) {
         $.post(
             URL + '/admin/login', {
                 user_name: myName,
                 password: myPwd,
             },
-            function(callback) {
+            function(res) {
                 callback(res);
+            }
+        )
+    },
+
+    // 退出请求
+    logOut: function(callback) {
+        $.post(
+            URL + '/admin/logout',
+            function(res) {
+                callback(res);
+                res.code == 200 ? location.href = './login.html' : '';
             }
         )
     }
