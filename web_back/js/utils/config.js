34015422cdde2321d7fd5cd348bf85f026cbc015
QiRu <916331669@qq.com>
2019-11-21 19:29:56 +0800

    初始化接口地址重构

---
+++ web_back/js/utils/config.js
@@ -0,0 +1,7 @@
+// 初始化接口默认地址
+var URL = 'http://localhost:8000';
+var baseUrl = {
+    loginUrl: URL + '/admin/login',
+    logOutUrl: URL + '/admin/logout',
+    getInfoUrl: URL + '/admin/getuser',
+}
--- web_back/js/utils/user.js
+++ web_back/js/utils/user.js
@@ -1,10 +1,8 @@
-// 初始化接口默认地址
-var URL = 'http://localhost:8000';
 var user = {
     // 登录请求
     login: function(myName, myPwd, callback) {
         $.post(
-            URL + '/admin/login', {
+            baseUrl.loginUrl, {
                 user_name: myName,
                 password: myPwd,
             },
@@ -17,7 +15,7 @@ var user = {
     // 退出请求
     logOut: function(callback) {
         $.post(
-            URL + '/admin/logout',
+            baseUrl.logOutUrl,
             function(res) {
                 callback(res);
             }
@@ -27,7 +25,7 @@ var user = {
     // 获取管理员信息并显示在主页中
     getInfo: function(callback) {
         $.get(
-            URL + '/admin/getuser',
+            baseUrl.getInfoUrl,
             function(res) {
                 callback(res);
             }
